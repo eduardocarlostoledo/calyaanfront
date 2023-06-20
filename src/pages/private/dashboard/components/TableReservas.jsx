ecbbf37b9a7a2bb2a4bf46b382f7cbd7f5b9178a
LeanKrn <leandrotomask@gmail.com>
2023-06-19 22:56:07 -0300

    Arregle la barra de busqueda

--- src/pages/private/dashboard/components/TableReservas.jsx
+++ src/pages/private/dashboard/components/TableReservas.jsx
@@ -1,6 +1,6 @@
 import fileDownload from "js-file-download";
 import React from "react";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { AiOutlineCloudDownload } from "react-icons/ai";
 import { BsPersonCircle } from "react-icons/bs";
 import { MdKeyboardArrowDown } from "react-icons/md";
@@ -17,9 +17,10 @@ import ModalUserInfo from "./ModalUserInfo";
 const TableReservas = () => {
   const { paginado, setLimite, limite, pagina, setPagina, loading } =
     useGetDateTableReservas();
-
   const [userState, setUserState] = useState({});
   const [modal, setModal] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filtrado, setFiltrado] = useState([]);
 
   const dispatch = useDispatch();
 
@@ -35,6 +36,33 @@ const TableReservas = () => {
       });
   };
 
+  const FilterR = useMemo(
+    (Value) => {
+      if (!paginado || !paginado.resultados) {
+        return [];
+      }
+      setFiltrado(
+        paginado.resultados.filter(
+          (item) =>
+            item.cliente_email
+              ?.toLowerCase()
+              .includes(searchTerm?.toLowerCase()) ||
+            item._id?.toLowerCase().includes(searchTerm?.toLowerCase()) ||
+            item.cliente_apellido
+              ?.toLowerCase()
+              .includes(searchTerm?.toLowerCase()) ||
+            item.cliente_nombre
+              ?.toLowerCase()
+              .includes(searchTerm?.toLowerCase()) ||
+            ` ${item.cliente_nombre} ${item.cliente_apellido}`
+              ?.toLowerCase()
+              .includes(searchTerm?.toLowerCase())
+        )
+      );
+    },
+    [searchTerm]
+  );
+
   const handleState = () => {
     dispatch(estadoAction());
   };
@@ -48,8 +76,6 @@ const TableReservas = () => {
     setModal(!modal);
   };
 
-  console.log(paginado)
-
   return (
     <>
       <div className="py-8">
@@ -60,7 +86,7 @@ const TableReservas = () => {
               <div className="relative">
                 <select
                   value={limite}
-                  onChange={(e) => setLimite(e.target.value)}
+                  onChange={(e) => setLimite(e.target.value || 50)}
                   className="appearance-none h-full rounded-l border block a w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 >
                   <option value={5}>5</option>
@@ -84,18 +110,20 @@ const TableReservas = () => {
               </div>
             </div>
 
-            <div className="block relative">
+            <div className="block relative w-80">
               <p className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
                 <svg
                   viewBox="0 0 24 24"
-                  className="h-4 w-4 fill-current text-gray-500"
+                  className="h-4 w-4 fill-current text-gray-500 "
                 >
                   <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
                 </svg>
               </p>
               <input
-                placeholder="Buscar"
+                placeholder="Buscar Nombre/ Apellido/ email/ id"
                 className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
             </div>
           </div>
@@ -119,7 +147,7 @@ const TableReservas = () => {
             </button>
           </div>
         </div>
-        <div className="-mx-4 px-4 sm:px-8 py-4 overflow-x-auto">
+        <div className="-mx-4 px-4 sm:px-8 py-4 overflow-x-auto ">
           <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
             <table className="min-w-full leading-normal">
               <thead>
@@ -170,44 +198,44 @@ const TableReservas = () => {
                     </td>
                   </tr>
                 ) : (
-                  paginado?.resultados?.map((reserva) => (
+                  filtrado?.map((reserva) => (
                     <tr key={reserva._id}>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm ">
                         <div className="flex items-center">{reserva._id}</div>
                       </td>
 
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <div className="flex items-center">
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm ">
+                        <div className="flex justify-start">
                           <div className="flex-shrink-0 w-10 h-10">
                             {reserva?.cliente?.img ? (
                               <img
-                                className="w-full h-full rounded-full"
+                                className="w-full h-full rounded-full cursor-pointer"
                                 src={user?.img}
                                 alt="Imagen de usuario"
                                 onClick={() => handleUser(reserva?.cliente_id)}
                               />
                             ) : (
                               <BsPersonCircle
-                                className="w-full h-full text-gray-300"
+                                className="w-full h-full text-gray-300 cursor-pointer"
                                 onClick={() => handleUser(reserva?.cliente_id)}
                               />
                             )}
                           </div>
                         </div>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <div className="flex items-center">
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm ">
+                        <div className="flex justify-start">
                           <div className="flex-shrink-0 w-10 h-10">
                             {reserva?.profesional?.img ? (
                               <img
-                                className="w-full h-full rounded-full"
+                                className="w-full h-full rounded-full cursor-pointer"
                                 src={user?.img}
                                 alt="Imagen de usuario"
                                 onClick={() => handleUser(reserva?.cliente_id)}
                               />
                             ) : (
                               <BsPersonCircle
-                                className="w-full h-full text-gray-300"
+                                className="w-full h-full text-gray-300 cursor-pointer"
                                 onClick={() => handleUser(reserva?.cliente_id)}
                               />
                             )}
@@ -228,12 +256,17 @@ const TableReservas = () => {
                         >
                           <p
                             aria-hidden
-                            className={`absolute inset-0 opacity-50 rounded-full ${reserva.estadoPago === "approved" ? "bg-green-200" : reserva.estadoPago === "pending" ? "bg-yellow-200" : "bg-red-200"}`}
+                            className={`absolute inset-0 opacity-50 rounded-full ${
+                              reserva.estadoPago === "approved"
+                                ? "bg-green-200"
+                                : reserva.estadoPago === "pending"
+                                ? "bg-yellow-200"
+                                : "bg-red-200"
+                            }`}
                           ></p>
                           <p className="relative">{reserva.estadoPago}</p>
                         </div>
                       </td>
-
 
                       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                         <p className="text-gray-900 whitespace-no-wrap">
@@ -241,24 +274,27 @@ const TableReservas = () => {
                         </p>
                       </td>
 
-
                       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                         <p className="text-gray-900 whitespace-no-wrap">
                           {reserva.hora_servicio}
                         </p>
                       </td>
 
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm flex">
-                    
-                        <Link to={`/resumen-admin/${reserva._id}`} className="text-gray-900 whitespace-no-wrap">
+                      <td className="px-5 py-5 h-full border-b border-gray-200 bg-white text-sm flex justify-center">
+                        <Link
+                          to={`/resumen-admin/${reserva._id}`}
+                          className="text-gray-900 whitespace-no-wrap p-3"
+                        >
                           Ver reserva
                         </Link>
 
-                        <Link to={`/reservar?id=${reserva._id}`} className="text-gray-900 whitespace-no-wrap">
-                          Ver reserva Formulario
+                        <Link
+                          to={`/reservar?id=${reserva._id}`}
+                          className="text-gray-900 whitespace-no-wrap ml-8 p-3"
+                        >
+                          Formulario
                         </Link>
                       </td>
-
                     </tr>
                   ))
                 )}
@@ -276,10 +312,11 @@ const TableReservas = () => {
                 <div className="inline-flex mt-2 xs:mt-0">
                   <button
                     onClick={() => pagina !== 1 && setPagina(pagina - 1)}
-                    className={`text-sm font-semibold py-2 px-4 rounded-r ${pagina !== 1
-                      ? "bg-gray-300 hover:bg-gray-400 text-gray-800"
-                      : "disabled:opacity-25"
-                      }`}
+                    className={`text-sm font-semibold py-2 px-4 rounded-r ${
+                      pagina !== 1
+                        ? "bg-gray-300 hover:bg-gray-400 text-gray-800"
+                        : "disabled:opacity-25"
+                    }`}
                     disabled={pagina !== 1 ? false : true}
                   >
                     Ant
@@ -288,10 +325,11 @@ const TableReservas = () => {
                     onClick={() =>
                       pagina < paginado.totalPaginas && setPagina(pagina + 1)
                     }
-                    className={`text-sm font-semibold py-2 px-4 rounded-r ${pagina < paginado.totalPaginas
-                      ? "bg-gray-300 hover:bg-gray-400 text-gray-800"
-                      : "disabled:opacity-25"
-                      }`}
+                    className={`text-sm font-semibold py-2 px-4 rounded-r ${
+                      pagina < paginado.totalPaginas
+                        ? "bg-gray-300 hover:bg-gray-400 text-gray-800"
+                        : "disabled:opacity-25"
+                    }`}
                     disabled={pagina < paginado.totalPaginas ? false : true}
                   >
                     Sig
@@ -304,7 +342,10 @@ const TableReservas = () => {
       </div>
 
       {modal && (
-        <ModalUserInfo userState={userState} handleModalView={handleModalView} />
+        <ModalUserInfo
+          userState={userState}
+          handleModalView={handleModalView}
+        />
       )}
     </>
   );
