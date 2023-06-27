b9d04b1b8944ebafb53a4db3a07e2f962a70f9a6
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2023-06-27 17:18:13 -0300

    deploy

--- src/pages/private/dashboard/pages/HorariosProfesionales.jsx
+++ src/pages/private/dashboard/pages/HorariosProfesionales.jsx
@@ -15,8 +15,10 @@ const HorarioProfessionalAntDesing = () => {
   const [error, setError] = useState(null);
   const [searchText, setSearchText] = useState("");
   const [editProduct, setEditProduct] = useState(0);
-  //   const change = useSelector((state) => state.professional.disponibilidad);
-  //separo los useEffect para que no se renderize todo junto
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+
+  
   useEffect(() => {
     dispatch(disponibilidadesTotalesGet())
       .then(() => setLoading(false))
@@ -129,6 +131,17 @@ const HorarioProfessionalAntDesing = () => {
     },
   ];
 
+  const onSelectChange = (selectedRowKeys) => {
+    setSelectedRowKeys(selectedRowKeys);
+  
+    // Obtener los datos de las filas seleccionadas
+    const selectedRows = filteredOrdenes.filter(
+      (orden) => selectedRowKeys.includes(orden.key)
+    );
+    console.log(selectedRows);
+  };
+
+  
   return (
     
     <div
@@ -169,6 +182,10 @@ const HorarioProfessionalAntDesing = () => {
           style={{ backgroundColor: "rgb(245, 245, 235)" }}
           columns={columns}
           dataSource={filteredOrdenes}
+          rowSelection={{
+            selectedRowKeys,
+            onChange: onSelectChange,
+          }}
         />
       </div>
 
@@ -184,6 +201,193 @@ const HorarioProfessionalAntDesing = () => {
 };
 
 export default HorarioProfessionalAntDesing;
+
+// import React, { useEffect, useState, useMemo } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Table, List, Spin, Alert, Tag, Input } from "antd";
+// import { AiFillSetting, AiOutlineClose } from "react-icons/ai";
+// import { BiEditAlt } from "react-icons/bi";
+// import moment from "moment";
+// import swal from "sweetalert";
+// import "./Ordenesantd.css";
+// import { disponibilidadesTotalesGet } from "../../../../redux/features/professionalSlice";
+// // import { newHourArray,localidadesLaborales } from "../../../../data";
+
+// const HorarioProfessionalAntDesing = () => {
+//   const dispatch = useDispatch();
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//   const [searchText, setSearchText] = useState("");
+//   const [editProduct, setEditProduct] = useState(0);
+//   //   const change = useSelector((state) => state.professional.disponibilidad);
+//   //separo los useEffect para que no se renderize todo junto
+//   useEffect(() => {
+//     dispatch(disponibilidadesTotalesGet())
+//       .then(() => setLoading(false))
+//       .catch((error) => setError(error.message));
+//   }, [dispatch]);
+
+//   const orders = useSelector(
+//     (state) => state.professional.disponibilidad || []
+//   );
+//   console.log(orders);
+//   //// Mapeo ordenes para agregar una key a cada fila
+//   const newProducts = orders?.map((product) => ({
+//     ...product,
+//     key: product._id,
+//   }));
+
+//   const filteredOrdenes = useMemo(() => {
+//     return newProducts?.filter((orden) => {
+//       const { creador, fecha, disponibilidad } = orden;
+//       const { telefono, email, nombre, apellido, localidadesLaborales, especialidad } =
+//         creador?.creador;
+
+//       const nombreCompleto = `${nombre} ${apellido}`.toLowerCase();
+
+//       const matchLocalidad = creador.localidadesLaborales?.some((localidad) =>
+//         localidad.toLowerCase().includes(searchText.toLowerCase())
+//       );
+
+//       const matchEspecialidad = creador.especialidad?.some((especialidades) =>
+//       especialidades.toLowerCase().includes(searchText.toLowerCase())
+//       );
+
+//       const matchHoraServicio = disponibilidad?.some((hora) =>
+//         hora.hora.toLowerCase().includes(searchText.toLowerCase())
+//       );
+
+//       return (
+        
+//         telefono?.includes(searchText) ||
+//         email?.includes(searchText) ||
+//         nombreCompleto.includes(searchText.toLowerCase()) ||
+//         (nombre?.toLowerCase().includes(searchText.toLowerCase()) &&
+//           apellido?.toLowerCase().includes(searchText.toLowerCase())) ||
+//         moment(fecha, "YYYY-MM-DD").format("YYYY-MM-DD").includes(searchText) ||
+//         matchHoraServicio ||
+//         matchLocalidad || 
+//         matchEspecialidad
+//       );
+//     });
+//   }, [newProducts, searchText]);
+
+//   const columns = [
+//     {
+//       title: "Nombre",
+//       dataIndex: "creador",
+//       //   sorter: (a, b) => a.id - b.id,
+//       defaultSortOrder: "descend",
+//       render: (creador) => (
+//         <p>
+//           <b>
+//             {creador.creador.nombre} {creador.creador.apellido}
+//           </b>
+//         </p>
+//       ),
+//     },
+//     {
+//       title: "Especialidad",
+//       dataIndex: "creador",
+//       sorter: (a, b) => a.id - b.id,
+//       defaultSortOrder: "descend",
+//       render: (text) =>
+//         text.especialidad.map((e) => <p> {e} </p>),
+      
+//     },    
+//     {
+//       title: "Telefono",
+//       dataIndex: "creador",
+//       //   sorter: (a, b) => a.id - b.id,
+//       defaultSortOrder: "descend",
+//       render: (creador) => <p>{creador.creador.telefono}</p>,
+//     },
+//     {
+//       title: "email",
+//       dataIndex: "creador",
+//       //   sorter: (a, b) => a.id - b.id,
+//       defaultSortOrder: "descend",
+//       render: (creador) => <p>{creador.creador.email}</p>,
+//     },
+//     {
+//       title: "Localidad",
+//       dataIndex: "creador",
+//       //   sorter: (a, b) => a.id - b.id,
+//       defaultSortOrder: "descend",
+//       render: (text) =>
+//         text.localidadesLaborales.map((localidad) => <p> {localidad} </p>),
+//     },
+//     {
+//       title: "Fecha",
+//       dataIndex: "fecha",
+//       //   sorter: (a, b) => a.id - b.id,
+//       defaultSortOrder: "descend",
+//       render: (text) => <p>{text}</p>,
+//     },
+//     {
+//       title: "Hora",
+//       dataIndex: "disponibilidad",
+//       //   sorter: (a, b) => a.id - b.id,
+//       defaultSortOrder: "descend",
+//       render: (text) => text.map((localidad) => <p> {localidad.hora} </p>),
+//     },
+//   ];
+
+//   return (
+    
+//     <div
+//       style={{ textAlign: "center", alignItems: "center", overflow: "auto" }}
+//     >
+//       <p className="p">DISPONIBILIDAD PROFESIONAL</p>
+//       <p className="p">
+//         BUSQUEDA POR NOMBRE, APELLIDO, ESPECIALIDAD, TELEFONO, EMAIL, LOCALIDAD LABORAL, FECHA DE DISPONIBILIDAD, HORA DISPONIBLE.
+//       </p>
+      
+//       <h1 style={{ textAlign: "center", alignItems: "center" }}>
+//         <Input.Search
+//           placeholder="Buscar"
+//           onChange={(e) => setSearchText(e.target.value)}
+//           style={{
+//             width: 400,
+//             marginBottom: "0px",
+//             textAlign: "center",
+//             alignItems: "center",
+//           }}
+//         />
+//       </h1>
+//       {/* <p className="p">
+//         Puede ordenar ascendente o descendentemente con las flechas en Datos de
+//         la tabla
+//       </p> */}
+
+//       <div
+//         style={{
+//           margin: "0px",
+//           marginLeft: "0px",
+//           marginTop: "0px",
+//           padding: "0px",
+//           // width: '100%', height: '1000px', overflowY: "auto", overflowX: 'auto'
+//         }}
+//       >
+//         <Table
+//           style={{ backgroundColor: "rgb(245, 245, 235)" }}
+//           columns={columns}
+//           dataSource={filteredOrdenes}
+//         />
+//       </div>
+
+//       {/* <div style={{ marginTop: "80px", padding: "20px" }}>
+//         <Table
+//           style={{ backgroundColor: "rgb(245, 245, 235)" }}
+//           columns={columns}
+//           dataSource={orders}
+//         />
+//       </div> */}
+//     </div>
+//   );
+// };
+
+// export default HorarioProfessionalAntDesing;
 
 // import React, { useEffect, useState, useMemo } from "react";
 // import { useDispatch, useSelector } from "react-redux";
