a98ecc9f559effc2946035416378398edd6faec0
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2023-08-03 17:15:21 -0300

    se agregan especialidadeshabilitadas

--- src/data/index.js
+++ src/data/index.js
@@ -347,12 +347,17 @@ export const especialidadesHabilitadas = [
   "Masaje reductor paquete x 4 (con camilla)",
   "Masaje reductor paquete x 6",
   "Masaje reductor paquete x 6 (con camilla)",
-  "masajes reductores x 10",
+  "Masajes reductores paquete x 10",
   "Masaje reductor paquete x 10 (con camilla)",
   "Masajes reductores paquete x 20",
   "Masaje reductor paquete x 20 (con camilla)",
   "Drenaje linfático",
   "Drenaje linfático x 10 sesiones",
+  "Masaje + spa + piedras",
+  "Masaje relajante",
+  "Masaje descontracturante",
+  "Masaje Post Operatorio + Aparatologia x 10 sesiones",
+  "Masaje + Chocolaterapia + exfoliación corporal",
 ];
 
 
