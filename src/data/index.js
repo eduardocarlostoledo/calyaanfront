359fa3f1d0166765f0663033dec78b87edd98aeb
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2023-09-27 21:45:37 -0300

    agrega paquetes descontracturante x10 y x6

--- src/data/index.js
+++ src/data/index.js
@@ -372,6 +372,8 @@ export const especialidadesHabilitadas = [
   "Masaje Reductor de Valoracion",
   "Piel de porcelana",
   "Limpieza facial profunda",
+  "Paquete Masaje descontracturante x 10 sesiones",
+  "Paquete Masaje descontracturante x 6 sesiones",
 ];
 
 
