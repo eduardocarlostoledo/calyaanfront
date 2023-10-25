8b69c7e0da81d6a10bfba1d6e046a08c51462886
eduardo <eduardocarlostoledo@gmail.com>
2023-10-25 10:19:11 -0300

    fix roles

--- src/helpers/Logic/roles.js
+++ src/helpers/Logic/roles.js
@@ -1,5 +1,5 @@
 export const ROLES = {
     ADMIN: "TTTjGp7Lb$2cE9mW*!hRnX",
-    PROFESIONAL: "XXXzP@4qVx#8nT*oB",
-    CLIENTE: "yK&3wNs$5rM1lA"
+    PROFESIONAL: "XXXzP@4qVx#8nT*oBx",
+    CLIENTE: "xyK&3wNs$5rM1lAx"
 }
